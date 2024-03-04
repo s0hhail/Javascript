258c0b397a39ca3241005a2e425b886060cbe836
Md Sohail Afridi <149901698+s0hhail@users.noreply.github.com>
2024-03-04 17:20:35 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,4 +24,49 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//************ Operations **************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2) 
+// console.log(2-2) 
+// console.log(2*2)                                       //node 01_basics/03_conversionOperation.js
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Sohail"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log( (3 + 4) * 5 % 3)
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// let x = ++gameCounter
+let y = gameCounter++
+console.log(gameCounter)
+// console.log(x)
+console.log(y)
+
+
+
+//link to study x++ & ++x
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
