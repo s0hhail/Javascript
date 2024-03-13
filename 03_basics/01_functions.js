468dcf6376527d3695fc2c6c6312ec20a4e87017
Md Sohail Afridi <149901698+s0hhail@users.noreply.github.com>
2024-03-13 06:13:37 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -9,7 +9,7 @@ function sayMyName () {
 
 // sayMyName()
 
-// function addTwoNumbers (number1, number2) {               
+// function addTwoNumbers (number1, number2) {
 //     console.log(number1 + number2)
 // }
 
@@ -31,4 +31,37 @@ function userLoginMessage (username = "Mohammad") {
     }
     return `${username} just logged in`
 }
-console.log(userLoginMessage("Sohail"))
+// console.log(userLoginMessage("Sohail"))
+
+
+function calculateCartPrice (...num1) {
+    let sum = 0
+    for (let index = 0; index < num1.length; index++) {
+        sum = sum + num1[index] 
+    }
+    return sum
+}
+// console.log(calculateCartPrice(300,400,700,600,500,500))
+
+const user = {
+    username : "Sohail",
+    price : 199
+}
+
+function handleObjects (anyObject) {
+    console.log(`User is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+// handleObjects(user)
+// handleObjects ({
+//     username : "Afridi",
+//     price : 399
+// })
+
+const arr = [100,200,400,500,600,900]
+
+function returnSecondValue (array) {
+    return array[1]
+}
+// console.log(returnSecondValue(arr))
+console.log(returnSecondValue([10,20,40,80,70]))
